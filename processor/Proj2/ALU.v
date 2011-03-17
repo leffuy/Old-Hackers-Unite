2a5ada3139051f27a242dbc952014ad0a0145660
James Cline <james.cline@gatech.edu>
2011-03-17 05:57:11 -0500

    Working 85 mhz

--- processor/Proj2/ALU.v
+++ processor/Proj2/ALU.v
@@ -17,21 +17,36 @@ module ALU(A,B,CTL,OUT);
   input  [(BITS-1):0] A,B;
   output [(BITS-1):0] OUT;
   wire signed [(BITS-1):0] A,B;
-  reg signed [(BITS-1):0] tmpout;
-  always @(A or B or CTL) begin
+  reg signed [(BITS-1):0] tmpout, addsubout, cmpout, logout;
+  reg signed [(BITS):0] x,y,sum;
+  wire addsub = ~CTL[3]&~CTL[2], cmp = ~CTL[3]&CTL[2];
+  reg cin;
+  always @(A or B or CTL or addsub or cmp or y) begin
+    cin = 1'b0;
+    x={A,1'b1}; 
+    y={B,cin};
+    if(CTL[0]) begin
+	    cin = 1'b1;
+	    y={~B,cin};
+    end
+    sum = (x+y);
+    addsubout = sum[BITS:1];
+    cmpout[0] = addsubout[(BITS-1)];
+
     case(CTL)
-      CMD_ADD:  tmpout = A+B;
-      CMD_SUB:  tmpout = A-B;
-      CMD_LT:   tmpout = (A<B);
-      CMD_LE:   tmpout = (A<=B);
-      CMD_AND:  tmpout = A&B;
-      CMD_OR:   tmpout = A|B;
-      CMD_XOR:  tmpout = A^B;
-      CMD_NAND: tmpout = ~(A&B);
-      CMD_NOR:  tmpout = ~(A|B);
-      CMD_NXOR: tmpout = ~(A^B);
-      default:  tmpout = {BITS{1'bX}};
+      CMD_AND:  logout = A&B;
+      CMD_OR:   logout = A|B;
+      CMD_XOR:  logout = A^B;
+      CMD_NAND: logout = ~(A&B);
+      CMD_NOR:  logout = ~(A|B);
+      CMD_NXOR: logout = ~(A^B);
+      default:  logout = {BITS{1'bX}};
     endcase
+
+    tmpout = (addsubout & {{(BITS-1){addsub}},addsub}) |  (logout & {{(BITS-1){CTL[3]}},CTL[3]}) |  (cmpout & {{(BITS-1){cmp}},cmp}); 
+
   end
+
+
   assign OUT=tmpout;
 endmodule
